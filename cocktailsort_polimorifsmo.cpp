3edd22261bad403752b68ee3b43907b6fa0660bf
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-09-09 16:04:14 -0500

    Add files via upload

---
+++ cocktailsort_polimorifsmo.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void generar(int *a, long long tam){
+    for(int i = 0; i < tam; i++){
+        a[i] = rand()%10000;
+    }
+}
+
+void imprimir(int *a, int tam){
+    cout << "[";
+    for(int i = 0; i < tam; i++){
+        if(i != tam-1){
+            cout << a[i] << ",";
+        }
+        else{
+            cout << a[i];
+        }
+    }
+    cout << "]" << endl;
+}
+
+void swap(int *a, int *b){
+    int *temp = a;
+    *a = *b;
+    *b = *temp;
+}
+
+class cocktail{
+private:
+    virtual bool comparar(int *a, int *b) = 0;
+public:
+    cocktail(){};
+    void ordenar(int *p, int *q){
+        int *temp=p;
+        bool intercambio=1;
+        while(intercambio==1&&q>p){
+            intercambio=0;
+            for(;p<q;p++){
+                if(comparar(p,p+1)){
+                    swap(p,p+1);
+                    intercambio=1;
+                }
+            }
+            for(;q>temp;q--){
+                if(comparar(q-1,q)){
+                    swap(q-1,q);
+                    intercambio=1;
+                }
+            }
+            temp=q;
+            q=p;
+            temp++;
+            p=temp;
+            q--;
+        }
+    };
+
+};
+
+class mayor: public cocktail{
+public:
+    bool comparar(int *a, int *b){
+        return (*a > *b);
+    }
+};
+
+class menor: public cocktail{
+public:
+    bool comparar(int *a, int *b){
+        return (*a < *b);
+    }
+};
+
+int main(){
+    srand(time(NULL));
+    int *a = new int[1000000];
+
+    for(long long i = 0; i < 1000000; i++){
+        a[i] = rand()%10000;
+    }
+
+    mayor valor;
+    valor.ordenar(a,(a+1000000-1));
+    imprimir(a,1000000);
+    return 0;
+}
---
+++ cocktailsort_punt.funcion.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void swap(int *a, int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+void generar(int *a, int tam){
+    srand(time(NULL));
+    for(int i = 0; i < tam; i++){
+        a[i] = rand()%50;
+    }
+}
+
+void imprimir(int *a, int tam){
+    cout << "[";
+    for(int i = 0; i < tam; i++){
+        if(i != tam-1){
+            cout << a[i] << ",";
+        }
+        else{
+            cout << a[i];
+        }
+    }
+    cout << "]" << endl;
+}
+
+bool menor(int *a, int *b){
+	return *a>*b;
+}
+
+bool mayor(int *a, int *b){
+	return *a<*b;
+}
+
+void CoctailSort(int *p, int *q, bool (*comparar)(int *a, int *b)){
+	int *temp=p;
+	bool intercambio=1;
+	while(intercambio==1&&q>p){
+		intercambio=0;
+		for(;p<q;p++){
+			if(comparar(p,p+1)){
+				swap(p,p+1);
+				intercambio=1;
+			}
+		}
+		for(;q>temp;q--){
+			if(comparar(q-1,q)){
+				swap(q-1,q);
+				intercambio=1;
+			}
+		}
+		temp=q;
+		q=p;
+		temp++;
+		p=temp;
+		q--;
+	}
+}
+
+int main()
+{
+    int tama;
+    cout << "Ingrese el largo: ";
+    cin >> tama;
+    tama = 3;
+    int a[tama];
+
+    generar(a,tama);
+    imprimir(a,tama);
+
+    CoctailSort(a,a+tama,menor);
+    imprimir(a,tama);
+
+    return 0;
+}
