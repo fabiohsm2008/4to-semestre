51ca937fe6ffb1b784366833e5110ad4eba1e741
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-09-16 15:57:45 -0500

    Add files via upload

--- lista_enlazada.cpp
+++ lista_enlazada.cpp
@@ -5,41 +5,64 @@ using namespace std;
 template <typename T>
 
 class nodo{
-private:
+public:
     T Dato;
     nodo* next;
-public:
+    nodo(){};
     nodo(T s, nodo* sgt){
         Dato = s;
         next = sgt;
     };
     ~nodo(){};
-    void imprimir(){
-        nodo *temp = next;
-        while(temp != NULL){
-            cout << Dato << " ";
-            temp = temp->next;
-        }
-    };
-    bool buscar(T a, nodo** p){
-        nodo *temp = next;
-        while(temp != NULL){
-            if(*temp->Dato == a){
-                *p = temp;
-                return true;
-            }
-        }
-        return false;
-    };
-    void insertar(T a){
+};
 
+template <typename Q>
+class lista{
+private:
+    nodo <Q> *head;
+public:
+    lista(nodo <Q> x){
+        head = &x;
     };
-    void borrar(T a){
+    void imprimir(){
+        nodo<Q> *temp = head;
+        while(temp != NULL){
+            cout << (temp)->Dato << " ";
+            temp = (temp)->next;
+        }
+    };
+    bool buscar(Q a, nodo<Q>**& p){
+        nodo<Q> *guardar = head;
+        while(a > (*p)->Dato && (*p)->next != NULL){
+            *p = (*p)->next;
+        }
+        head = guardar;
+        if((*p)->Dato == a){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    };
+    void insertar(Q a){
+        nodo<Q> **temp = &head;
+        if(!buscar(a,temp)){
+            nodo<Q> *nuevo = new nodo<Q>(a,(*temp)->next);
+            (*temp)->next = nuevo;
+        }
+    };
+    void borrar(Q a){
 
     };
 };
 
 int main(){
     nodo<int> head(0,NULL);
+    lista<int> prueba(head);
+    prueba.insertar(2);
+    prueba.insertar(3);
+    prueba.insertar(4);
+    prueba.insertar(5);
+    prueba.imprimir();
     return 0;
 }
