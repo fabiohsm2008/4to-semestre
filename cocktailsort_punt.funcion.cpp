5d68abb33d67791ff3beafae664e862505fc7d83
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-09-30 12:04:26 -0500

    Add files via upload

--- cocktailsort_punt.funcion.cpp
+++ cocktailsort_punt.funcion.cpp
@@ -34,35 +34,40 @@ bool menor(int *a, int *b){
 	return (*a > *b);
 }
 
-bool mayor(int
-           *a, int *b){
+bool mayor(int *a, int *b){
 	return (*a < *b);
 }
 
-void CoctailSort(int *p, int *q, bool (*comparar)(int *a, int *b)){
-	int *temp=p;
-	bool intercambio=1;
-	while(intercambio==1&&q>p){
-		intercambio=0;
-		for(;p<q;p++){
-			if(comparar(p,p+1)){
-				swap(p,p+1);
-				intercambio=1;
-			}
-		}
-		for(;q>temp;q--){
-			if(comparar(q-1,q)){
-				swap(q-1,q);
-				intercambio=1;
-			}
-		}
-		temp=q;
-		q=p;
-		temp++;
-		p=temp;
-		q--;
-	}
-}
+class cocktail{
+public:
+    bool (*comparar)(int *a, int *b);
+    cocktail(bool (*orden)(int *a, int *b)){comparar = orden;};
+    void ordenar(int *p, int *q){
+        int *temp=p;
+        bool intercambio=1;
+        while(intercambio==1&&q>p){
+            intercambio=0;
+            for(;p<q;p++){
+                if(comparar(p,p+1)){
+                    swap(p,p+1);
+                    intercambio=1;
+                }
+            }
+            for(;q>temp;q--){
+                if(comparar(q-1,q)){
+                    swap(q-1,q);
+                    intercambio=1;
+                }
+            }
+            temp=q;
+            q=p;
+            temp++;
+            p=temp;
+            q--;
+        }
+    };
+
+};
 
 int main()
 {
@@ -70,9 +75,10 @@ int main()
     int *a = new int[100000];
 
     generar(a,100000);
-    //imprimir(a,tama);
-    CoctailSort(a,a+100000-1,menor);
-    //imprimir(a,tama);
+    //imprimir(a,100000);
+    cocktail prueba(menor);
+    prueba.ordenar(a,a+100000-1);
+    //imprimir(a,100000);
 
     return 0;
 }
