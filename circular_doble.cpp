0657f80216421949c87490119ba53eb50bfeb5fc
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-10-28 15:50:14 -0500

    Update circular_doble.cpp

--- circular_doble.cpp
+++ circular_doble.cpp
@@ -16,8 +16,10 @@ class circular{
 public:
     nodo<T> *head;
     circular(){head = NULL;};
-    bool buscar(T dato, nodo<T> *actual){
+    bool buscar(T dato, nodo<T> *&actual){
+        actual = head;
         if(head->valor == dato) return 1;
+		actual = head->next;
         while(actual != head && actual->valor < dato){
             actual = actual->next;
         }
@@ -30,38 +32,71 @@ public:
             head->prev = head;
             return 1;
         }
-        nodo<T> *actual = head->next;
+        if (dato < head->valor){
+            nodo<T> *nuevo = new nodo<T>(dato,head->prev,head);
+            head->prev->next = nuevo;
+            head->prev = nuevo;
+            head = nuevo;
+            return 1;
+        }
+        nodo<T> *actual;
         if(buscar(dato, actual)) return 0;
         nodo<T> *nuevo = new nodo<T>(dato,actual->prev,actual);
         actual->prev->next = nuevo;
         actual->prev = nuevo;
         return 1;
     }
+	bool borrar(T dato){
+		nodo<T> *actual;
+		if(!buscar(dato,actual)) return 0;
+		if(actual == head){
+			head->prev->next = head->next;
+			head->next->prev = head->prev;
+			head = head->next;
+			delete actual;
+			return 1;
+		}
+		nodo<T> *temp = actual;
+		actual->prev->next = actual->next;
+		actual->next->prev = actual->prev;
+		delete temp;
+		return 1;
+		
+	}
     void imprimir(){
         nodo<T> *actual = head;
         cout << actual->valor << " ";
-        actual = actual->prev;
+        actual = actual->next;
         while(actual != head){
             cout << actual->valor << " ";
-            actual = actual->prev;
+            actual = actual->next;
         }
         cout << endl;
     }
 };
 
+void Josephus(circular<int> temp,int n, int k){
+	for(int i = 1; i <= n; i++){
+		temp.insertar(i);
+	};
+	nodo<int> *puntero = temp.head->prev;
+	temp.imprimir();
+	while(temp.head->next->next != temp.head){
+		for(int j = 0; j < k; j++){
+			puntero = puntero->next;
+		}
+		puntero = puntero->next;
+		temp.borrar(puntero->prev->valor);
+		puntero = puntero->prev;
+		temp.imprimir();
+	}
+	cout << "Primer sobreviviente = " << temp.head->valor << endl;
+	cout << "Segundo sobreviviente = " << temp.head->next->valor;
+	
+}
+
 int main(){
     circular<int> temp;
-    temp.insertar(1);
-    temp.imprimir();
-    temp.insertar(2);
-    temp.imprimir();
-    temp.insertar(3);
-    temp.imprimir();
-    temp.insertar(4);
-    temp.imprimir();
-    temp.insertar(5);
-    temp.imprimir();
-    temp.insertar(6);
-    temp.imprimir();
+	Josephus(temp, 40, 3);
     return 0;
 }
